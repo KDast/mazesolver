ec39be3134a29e2e5e2ab389513dee009fd35451
KDast <daoustkevin07@gmail.com>
2025-03-15 18:41:34 -0400

    Cell class

--- graphics.py
+++ graphics.py
@@ -41,17 +41,58 @@ class Line():
 
     
 class Cell():
-    def __init__(self, ):
+    def __init__(self, win):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self.win = win
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
 
-    def draw(self, top_left, bot_right):
+
+
+    def draw(self, x1, y1, x2, y2,):
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        top_left = Point(self._x1, self._y1)
+        bottom_right = Point(self._x2, self._y2)
+        top_right = Point(self._x2, self._y1)
+        bottom_left = Point(self._x1, self._y2)
         if self.has_left_wall == True:
+            left_wall = Line(top_left, bottom_left)
+            self.win.draw_line(left_wall, "purple") 
+        if self.has_right_wall == True:
+            right_wall = Line(top_right, bottom_right)
+            self.win.draw_line(right_wall, "purple") 
+        if self.has_bottom_wall == True:
+            bottom_wall = Line(bottom_left, bottom_right)
+            self.win.draw_line(bottom_wall, "purple") 
+        if self.has_top_wall == True:
+            top_wall = Line(top_left, top_right)
+            self.win.draw_line(top_wall, "purple") 
 
+    def draw_move(self, to_cell, undo=False):
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
 
-    
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half_length2 + to_cell._x1
+        y_center2 = half_length2 + to_cell._y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, fill_color)
 
 
        
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from graphics import *
 
 def main():
     win = Window(800, 600)
-    l = Line(Point(50, 50), Point(500, 150))
-    win.draw_line(l, "purple")         
+    c1 = Cell(50, 50, 100, 100, win)
+    c1.has_left_wall = False
+    c1.draw()         
     win.wait_for_close()
 
 
